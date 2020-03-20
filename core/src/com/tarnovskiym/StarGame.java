66ef73e6f274b173c20d3b66947fe31f1781b80c
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-03-20 12:10:11 +0700

    test Commit

--- core/src/com/tarnovskiym/StarGame.java
+++ core/src/com/tarnovskiym/StarGame.java
@@ -2,6 +2,7 @@ package com.tarnovskiym;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -21,7 +22,7 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 15, 15);
+		batch.draw(img, 15, 25);
 		batch.end();
 	}
 	
