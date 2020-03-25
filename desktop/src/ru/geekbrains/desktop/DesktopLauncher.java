a7e561fd916a1806b2980e1321968a17f53c53fd
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-03-25 21:09:09 +0700

    Lesson 2 part 2 Залил проект преподавателя

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -1,14 +1,12 @@
-package com.tarnovskiym.desktop;
+package ru.geekbrains.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.tarnovskiym.StarGame;
+import ru.geekbrains.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 840;
-		config.height = 840;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
